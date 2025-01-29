a4d6db407ec6c87179434bd84658077fc6826e0d
Sangameshwar killare <sangameshkillare@gmail.com>
2025-01-29 15:24:12 +0530

    first commit

---
+++ List_comprehension.py
@@ -0,0 +1,107 @@
+#------------------if element is grater than 3 alpha print it
+
+
+
+# list=["apple","Grapes","Mango"]
+# list2=[]
+# for i in list:
+    
+#     if len(i)>3:
+#         list2.append(i)
+# print(list2)
+
+
+#List comprhension
+
+# fruits=[fruit for fruit in list if len(fruit) > 3]
+# print(fruits)
+
+
+
+
+# -----------------------evn odd from the list 
+
+
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# list2 = []
+
+# for i in num:
+#     if i % 2 == 0:
+#         list2.append("Even")
+#     else:
+#         list2.append("Odd")
+
+# print(list2)
+
+
+
+#---------------------List comphrention 
+
+# num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# even=[x for x in num if x%2==0]
+# odd=[x for x in num if x%2!=0]
+# print(even,odd)
+
+
+
+#-----------------------srore even and odd in dict and tuple
+
+# evn_od = [{x:"even"} if x % 2 == 0 else {x:"odd "} for x in range(1,11)]
+# print(evn_od)
+
+
+
+
+#--------------------------upper lowr and length of list using list comprehension
+
+
+
+# list=["apple","Grapes","Mango"]
+
+# upper_case=[word.upper() for word in list]
+# print(upper_case)
+
+
+
+
+
+# list=["apple","Grapes","Mango"]
+
+# upper_case=[word.lower() for word in list]
+# print(upper_case)
+
+
+
+# list=["apple","Grapes","Mango"]
+
+# upper_case=[len(word) for word in list]
+# print(upper_case)
+
+
+#------------add special char after each element in list
+
+
+
+
+# list=["apple","Grapes","Mango"]
+# special_char=[ word + "@ "for word in list]
+# print(special_char)
+
+# list=["apple","Grapes","Mango"]
+# special_char=[ word + " Sangamesh "for word in list]
+# print(special_char)
+
+
+
+
+
+
+
+#------------------add 10 in each ele
+
+
+
+# list=[23,45,65,67,76]
+# list2=[i + 10 for i in list]
+# print(list2)
--- recursive_function.py
+++ recursive_function.py
@@ -1,14 +1,14 @@
 # #--------------------factorial
 
 
-def factorial(n):
-    if n==0:
-       return 1
-    else:
-       return n * factorial(n-1)
 
+# def fact(n):
+#    if n==0:
+#       return 1
+#    else:
+#       return n*fact(n-1)
+# print(fact(5))
 
-print(factorial(5))
 
 
 #-----------------------finonach series
@@ -22,3 +22,19 @@ print(factorial(5))
 
 # for i in range(5):
 #     print(fibo(i))
+
+
+
+
+#--------------------Sum of N numbers 
+
+
+
+# def sum(n):
+#    if n<=1:
+#       return False
+#    else:
+#       return n+sum(n-1)
+   
+# num=int(input("enter the number :"))
+# print(f"The sum of N number you enetered is {sum(num)}")
