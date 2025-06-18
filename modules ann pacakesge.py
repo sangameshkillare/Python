655a33259005dcf278086096b4dbffc53651469c
Sangameshwar killare <sangameshkillare@gmail.com>
2025-06-18 10:59:20 +0530

    update

---
+++ modules ann pacakesge.py
@@ -0,0 +1,25 @@
+# module is the collection of the file , pacake is the collection of modules 
+
+
+# import math
+# print(math.sqrt(14))
+
+import math as  m  #alice function 
+# print(m.sqrt(16)) 
+
+# print(m.pow(5,2))
+# print(m.log(5))
+# print(m.pi)
+
+
+# import numpy as np
+
+import os
+print(os.getcwd())
+
+from datetime import datetime
+now=datetime.now()
+print(now.strftime("%d-%m-%y-%t"))
+
+
+# import pandas as pd
